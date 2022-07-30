b4b2daeaf93c1ec5b9f219eb6eb0125f7fabad87
chick_0 <chick_0@ch1ck.xyz>
2022-07-30 17:10:35 +0900

    set order by and limit with page

--- src/main/java/mong/RockPaperScissors/rank/application/RankService.java
+++ src/main/java/mong/RockPaperScissors/rank/application/RankService.java
@@ -5,6 +5,7 @@ import mong.RockPaperScissors.rank.domain.Rank;
 import mong.RockPaperScissors.rank.dto.RankRequest;
 import mong.RockPaperScissors.rank.infrastructure.RankJpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
@@ -17,6 +18,8 @@ public class RankService {
     }
 
     public List<Rank> getRankList(){
-        return rankJpaRepository.findAll();
+        return rankJpaRepository.findAllByOrderByScoreDesc(
+            PageRequest.of(0, 10)
+        );
     }
 }
--- src/main/java/mong/RockPaperScissors/rank/infrastructure/RankJpaRepository.java
+++ src/main/java/mong/RockPaperScissors/rank/infrastructure/RankJpaRepository.java
@@ -2,6 +2,10 @@ package mong.RockPaperScissors.rank.infrastructure;
 
 import mong.RockPaperScissors.rank.domain.Rank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface RankJpaRepository extends JpaRepository<Rank, Long> {
+    List<Rank> findAllByOrderByScoreDesc(Pageable pageable);
 }
